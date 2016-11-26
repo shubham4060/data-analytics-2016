dc20ec0315753b346937adf139aa3e09e62ef435
Rameshwar Bhaskaran <rameshwar.zorro@gmail.com>
2016-11-26 22:30:23 +0530

    Added a version of Naive Bayes

--- project/DAProject4.R
+++ project/DAProject4.R
@@ -1,4 +1,5 @@
-churnData <- read.csv("data.csv",stringsAsFactors = F)
+readData <- read.csv("data.csv",stringsAsFactors = F)
+churnData <-readData
 #View(churnData)
 
 churnData$churn[churnData$churn == " True"] <- 1
@@ -45,3 +46,22 @@ library(caret)
 inTrain = createDataPartition(churnData$churn, p=2/3, list=FALSE)
 dfTrain=churnData[inTrain,]
 dfTest=churnData[-inTrain,]
+
+trainX = dfTrain[,2:20]
+testX = dfTest[,2:20]
+dfTest$churn <- as.factor(dfTest$churn)
+dfTrain$churn <- as.factor(dfTrain$churn)
+#The training dataset is in dfTrain and the test dataset in #dfTest
+
+#Make a model for Naive Bayes
+model = train(trainX,dfTrain$churn,'nb',trControl=trainControl(method='cv',number=10)) # 10 fold cross-validation set
+# find the confusion matrix
+prediction = predict(model$finalModel,testX)$class
+truth = dfTest$churn
+result<- confusionMatrix(prediction, truth)
+precision <- result$byClass['Pos Pred Value']    
+recall <- result$byClass['Sensitivity']
+#accuracy is mentioned already as such
+
+
+
